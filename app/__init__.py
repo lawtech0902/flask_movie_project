ee9d1bb6bfd390d7abebb1bf37cb1a0bfd6a2def
Lawtech <584563542@qq.com>
2017-09-04 14:39:00 +0800

    电影弹幕

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ __date__ = '2017/8/13 下午9:24'
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.redis import FlaskRedis
 
 import pymysql
 import os
@@ -14,10 +15,12 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = '18037ef7318e4f79bc7b8f38eceba15a'
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users/")
 app.debug = True
 db = SQLAlchemy(app)
+rd = FlaskRedis(app)
 
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
